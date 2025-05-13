0d137a8a8fa70defb99bd91ae2f4dd134025b392
massivebird <33991373+massivebird@users.noreply.github.com>
2025-05-13 08:28:37 -0400

    feat: find best match

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,62 @@
-use image::{ImageBuffer, Rgb, Rgba};
+use image::{ImageBuffer, Rgb};
+
+struct Screen {
+    basename: String,
+    rgb_avg: Rgb<u8>,
+}
+
+impl Screen {
+    fn new(basename: &str) -> Self {
+        Self {
+            basename: basename.to_owned(),
+            rgb_avg: {
+                let img = image::open(format!("/home/penguino/Pictures/jumpking/{basename}"))
+                    .expect("Could not find test-image")
+                    .into_rgb8();
+
+                rgb_avg(&img)
+            },
+        }
+    }
+}
 
 fn main() {
-    let basis = image::open("/home/penguino/Pictures/jumpking/redcrown05.png")
+    let screens = [
+        Screen::new("drain1.png"),
+        Screen::new("drain2.png"),
+        Screen::new("redcrown5.png"),
+        Screen::new("redcrown4.png"),
+        Screen::new("redcrown3.png"),
+    ];
+
+    let perp = image::open("/home/penguino/Pictures/screenshots/05-13-2025T08-27-54.png")
         .expect("Could not find test-image")
-        .into_rgba8();
-
-    let basis_avg = rgb_avg(&basis);
-
-    dbg!(basis_avg);
-
-    let perp = image::open("/home/penguino/Pictures/screenshots/05-13-2025T07-21-48.png")
-        .expect("Could not find test-image")
-        .into_rgba8();
+        .into_rgb8();
 
     let perp_avg = rgb_avg(&perp);
 
-    dbg!(perp_avg);
+    let mut best_screen = &screens[0];
+    let mut best_score = cosine_similarity(best_screen.rgb_avg.0, perp_avg.0);
 
-    let cosine_similarity: f32 = {
-        // Cosine similarity of two vectors is the dot product of the vectors
-        // divided by the product of their lengths.
+    for screen in screens.iter().skip(1) {
+        let cosine_similarity = cosine_similarity(screen.rgb_avg.0, perp_avg.0);
 
-        let mut dot_product: u16 = 0;
-
-        for i in 0..3 {
-            dot_product += u16::from(perp_avg[i]) * (u16::from(basis_avg[i]));
+        if cosine_similarity > best_score {
+            best_score = cosine_similarity;
+            best_screen = screen;
         }
+    }
 
-        let length_product = vector_length(basis_avg.0) * vector_length(perp_avg.0);
-
-        f32::from(dot_product) / length_product
-    };
-
-    dbg!(cosine_similarity);
+    println!("Best match: {}", best_screen.basename);
 }
 
-fn rgb_avg(img: &ImageBuffer<Rgba<u8>, Vec<u8>>) -> Rgb<u8> {
+fn rgb_avg(img: &ImageBuffer<Rgb<u8>, Vec<u8>>) -> Rgb<u8> {
     let mut mode: Rgb<u8> = Rgb([0, 0, 0]);
 
     let min = 60;
-    for Rgba { 0: rgb } in img
+    for Rgb { 0: rgb } in img
         .pixels()
-        .filter(|Rgba([r, g, b, _])| *r >= min || *g >= min || *b >= min)
+        .filter(|Rgb([r, g, b])| *r >= min || *g >= min || *b >= min)
     {
         for i in 0..3 {
             let sum: u16 = u16::from(mode[i]) + u16::from(rgb[i]);
@@ -60,4 +75,19 @@ fn vector_length(v: [u8; 3]) -> f32 {
     }
 
     f32::sqrt(inner as f32)
+}
+
+fn cosine_similarity(a: [u8; 3], b: [u8; 3]) -> f32 {
+    // Cosine similarity of two vectors is the dot product of the vectors
+    // divided by the product of their lengths.
+
+    let mut dot_product: u16 = 0;
+
+    for i in 0..3 {
+        dot_product += u16::from(a[i]) * (u16::from(b[i]));
+    }
+
+    let length_product = vector_length(b) * vector_length(a);
+
+    f32::from(dot_product) / length_product
 }
