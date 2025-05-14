0e34ec7c41f9bbdbc3d919a4405b0e852402d39a
massivebird <33991373+massivebird@users.noreply.github.com>
2025-05-13 23:49:27 -0400

    changed: algorithm to img_hash dist

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,27 @@
+use image::DynamicImage;
+use img_hash::{Hasher, HasherConfig, ImageHash, image};
 use std::borrow::Cow;
+use std::cell::OnceCell;
 use std::path::{Path, PathBuf};
-
-mod score;
 
 #[derive(Debug)]
 struct Screen {
     path: PathBuf,
+    image: DynamicImage,
+    hash: OnceCell<ImageHash>,
 }
 
 impl Screen {
     fn new(path: &Path) -> Self {
         Self {
             path: path.to_path_buf(),
+            image: image::open(path).expect("Could not find test-image"),
+            hash: OnceCell::new(),
         }
+    }
+
+    fn init_hash(&self, hasher: &Hasher) {
+        self.hash.set(hasher.hash_image(&self.image)).unwrap();
     }
 
     fn basename(&self) -> Cow<str> {
@@ -40,21 +49,30 @@ fn main() {
         .map(|d| Screen::new(&d.path()))
         .collect::<Vec<Screen>>();
 
+    let hasher = HasherConfig::new().to_hasher();
+
+    // Initialize hashes.
+    for screen in &screens {
+        screen.init_hash(&hasher);
+    }
+
     assert!(!screens.is_empty());
 
-    let mut best_screen = &screens[0];
-    let mut best_score = score::calc_score(&screens[0], &perp_img);
+    let perp_hash = hasher.hash_image(&perp_img);
+
+    let mut best_screen: Option<&Screen> = None;
+    let mut best_dist: Option<u32> = None;
 
     for screen in screens.iter().skip(1) {
+        let dist = perp_hash.dist(screen.hash.get().unwrap());
         dbg!(screen.basename());
-        let score = score::calc_score(screen, &perp_img);
-        dbg!(score);
+        dbg!(dist);
 
-        if score > best_score {
-            best_score = score;
-            best_screen = screen;
+        if best_dist.is_none_or(|best| best > dist) {
+            best_dist = Some(dist);
+            best_screen = Some(screen);
         }
     }
 
-    println!("Best match: {}", best_screen.basename());
+    println!("Best match: {}", best_screen.unwrap().basename());
 }
